a02370dcf9c29f0c0d744d8b041f9b3a5eb80c58
Maria Nevarez <mnevarez1220@gmail.com>
2020-07-15 13:42:48 -0600

    updated navbar and added CDN links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,10 +2,15 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo right">Logo</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
